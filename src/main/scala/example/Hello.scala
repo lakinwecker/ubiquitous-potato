3feed9ce43c6c1be6cc0e8110cceb21c66dd6085
Lakin Wecker <lakin.wecker@gmail.com>
2021-10-18 21:21:30 -0600

    Updated example of the API

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -5,6 +5,13 @@ object Hello extends Greeting with App {
   println(greeting)
   FairyStockfish.init()
   FairyStockfish.info()
+  println(FairyStockfish.validateFEN("shogi", "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1"))
+
+  //  The syntax for doing this is strange, due to the wrapper.
+  val moves = new FairyStockfish.VectorOfStrings()
+  // The UCI for this move is wrong, if we fix it, then it should work.
+  moves.push_back("3i4h")
+  println(FairyStockfish.getFEN("shogi", "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1", moves))
 }
 
 trait Greeting {
