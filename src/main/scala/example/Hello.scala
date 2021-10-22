37f4495fe429a21f86b8e2f18a2dd76ade711fed
Matt Tucker <matt.watkinsmedia.org>
2021-10-22 18:31:59 +0100

    Test FairyStockfish.gameResult api call

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -4,10 +4,11 @@ import org.playstrategy.FairyStockfish
 object Hello extends Greeting with App {
   println(greeting)
   FairyStockfish.init()
-  FairyStockfish.info()
+  //FairyStockfish.info()
   val variant = "shogi"
   val initFen = FairyStockfish.initialFen(variant)
-  val legalMoves = FairyStockfish.getLegalMoves(variant, initFen, new FairyStockfish.VectorOfStrings())
+  val emptyMoves = new FairyStockfish.VectorOfStrings()
+  val legalMoves = FairyStockfish.getLegalMoves(variant, initFen, emptyMoves)
   val uciMoves = List.range(0, legalMoves.size()).map(i => legalMoves.get(i).getString())
   println(uciMoves)
   uciMoves.map(m => println(FairyStockfish.getFEN(variant, initFen, new FairyStockfish.VectorOfStrings(m))))
@@ -15,9 +16,13 @@ object Hello extends Greeting with App {
   val piecesOnBoard = FairyStockfish.piecesOnBoard(variant, initFen)
   var first = piecesOnBoard.begin();
   while(!first.equals(piecesOnBoard.end())) {
-    println(s"${first.first().getString()} -> ${first.second().pieceInfo().name().getString()}")
+    println(s"${first.first().getString()} -> ${first.second().color()} - ${first.second().pieceInfo().id}")
     first = first.increment();
   }
+
+  //println(FairyStockfish.gameResult(variant, initFen, emptyMoves))
+  //println(FairyStockfish.isImmediateGameEnd(variant, initFen, emptyMoves))
+  //println(FairyStockfish.hasInsufficientMaterial(variant, initFen, emptyMoves).get1())
 }
 
 trait Greeting {
